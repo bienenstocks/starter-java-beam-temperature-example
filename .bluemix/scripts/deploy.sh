0521bbadddebfdc1b33a3b62b0c055e377e0104a
sarahb <sarahb@il.ibm.com>
2018-11-20 15:40:20 +0200

    script

--- .bluemix/scripts/deploy.sh
+++ .bluemix/scripts/deploy.sh
@@ -1,8 +1,5 @@
  #!/bin/bash
 
-
-      echo "region 1. ${PROD_REGION_ID} 2. $region_id"
-
       if ! [ -x "$(command -v bx)" ]; then
          curl -fsSL https://clis.ng.bluemix.net/install/linux | sh
       fi
@@ -12,7 +9,7 @@
       bx target --cf
 
       # get Streaming analytics credentials
-      bx resource service-key-delete "SA_${APP_NAME}"
+      bx resource service-key-delete "SA_${APP_NAME}" -f
       SA_KEY=$(bx resource service-key-create "SA_${APP_NAME}" Manager --instance-name "${SA_INSTANCE}")
 
 
@@ -28,13 +25,15 @@
 
       if [ $COS_INSTANCE ]; then
         # get COS credentials
+        bx resource service-key-delete "COS_${APP_NAME}" -f
+        COS_KEY=$(bx resource service-key-create "COS_${APP_NAME}" Manager --instance-name "${COS_INSTANCE}" --p {\"HMAC\":true})
+
+        bx plugin repo-plugins -r 'IBM Cloud'
         bx plugin install cloud-object-storage 
         COS=$(bx resource service-instance "${COS_INSTANCE}")
          COSCRN=$(echo ${COS} | awk 'BEGIN{FS=“crn: "} {print $2}' | awk '{ print $1 }')
          bx cos config —crn $COSCRN
         bx cos create-bucket --bucket ${APP_NAME} --region us-geo
-        bx resource service-key-delete "COS_${APP_NAME}"
-        COS_KEY=$(bx resource service-key-create "COS_${APP_NAME}" Manager --instance-name "${COS_INSTANCE}" --p {\"HMAC\":true})
 
         echo ",
          \"cos\": {
@@ -46,11 +45,24 @@
          }" >> vcap.json
       fi
 
+      if [ $MH_INSTANCE ]; then
+        # get MH credentials
+        bx resource service-key-delete "MH_${APP_NAME}" -f
+        MH_KEY=$(bx resource service-key-create "MH_${APP_NAME}" Manager --instance-name "${MH_INSTANCE}")
+
+        echo ",
+          \"messagehub\": {
+            \"user\": \"$(echo ${MH_KEY} | awk 'BEGIN{FS="user: "} {print $2}' | awk '{ print $1 }')\",
+            \"password\": \"$(echo ${MH_KEY} | awk 'BEGIN{FS="password: "} {print $2}' | awk '{ print $1 }')\",
+            \"kafka_brokers_sasl\": \"$(echo ${COS_KEY} | awk 'BEGIN{FS="kafka_brokers_sasl: "} {print $2}' | awk '{ print $1 }')\"
+          }" >>vcap.json
+      fi
+
       echo "}" >> vcap.json
       cat vcap.json
 
       java -cp ./example-starter-kit-0.1-SNAPSHOT.jar:./dependency/com.ibm.streams.beam-1.2.1/com.ibm.streams.beam/lib/com.ibm.streams.beam.translation.jar \
-      com.ibm.streams.beam.sample.temperature.TemperatureSample \
+           com.ibm.streams.beam.sample.temperature.TemperatureSample \
                --runner=StreamsRunner \
                --contextType=STREAMING_ANALYTICS_SERVICE \
                --vcapServices=./vcap.json \
